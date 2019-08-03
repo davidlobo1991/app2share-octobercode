25d6bb9271999d393001bbdbcda9bb012579fecd
David Gonzalez <david_gc_@hotmail.com>
2019-08-04 00:51:26 +0200

    Add map and add offers into partners

--- plugins/app2share/app/controllers/Offer.php
+++ plugins/app2share/app/controllers/Offer.php
@@ -5,11 +5,16 @@ use BackendMenu;
 
 class Offer extends Controller
 {
-    public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'    ];
+    public $implement = [
+        'Backend\Behaviors\ListController',
+        'Backend\Behaviors\FormController',
+        'Backend\Behaviors\ReorderController',
+    ];
     
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
+
 
     public function __construct()
     {
