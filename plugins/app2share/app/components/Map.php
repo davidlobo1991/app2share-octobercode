8279aef01e1b114298457b09b8fe957dcd221c2d
David Gonzalez <david_gc_@hotmail.com>
2019-08-03 15:30:47 +0200

    Add rules and validations

---
+++ plugins/app2share/app/components/Map.php
@@ -0,0 +1,19 @@
+<?php namespace App2share\App\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Map extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'map Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
