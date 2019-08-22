a0d0aad5581f826fdec3de8072ca909998aff303
David Gonzalez <david_gc_@hotmail.com>
2019-08-22 23:24:55 +0200

    Cambiado sw

--- themes/app2share/assets/js/sw.js
+++ themes/app2share/assets/js/sw.js
@@ -1,52 +1,64 @@
-self.addEventListener('push', function (/** PushEvent */ event) {
-    if (!(self.Notification && self.Notification.permission === 'granted')) {
-        return;
-    }
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v2';
+const assets = [
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+];
 
-    var showNotification = function (notification) {
-        return self.registration.showNotification(notification.title, {
-            body: notification.body,
-            icon: notification.icon || notification.image,
-            image: notification.image,
-            data: notification
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if(keys.length > size){
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
         });
-    };
+    });
+};
 
-    if (event.data) {
-        var json = event.data.json();
-
-        if (json) {
-            event.waitUntil(showNotification(json));
-        } else {
-            console.warn("Invalid push data received", event);
-        }
-    } else {
-        console.warn("Empty push received", event);
-    }
+// install event
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
 
-self.addEventListener('notificationclick', function (/** NotificationEvent */event) {
-    var notification = event.notification;
+// activate event
+self.addEventListener('activate', evt => {
+    //console.log('service worker activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
 
-    // Cerrar notificaciÃ³n
-    notification.close();
-
-    // URL que se quiere abrir
-    var urlToOpen = new URL((notification.data && notification.data.url) || '/', self.location.origin).href;
-
-    event.waitUntil(clients.matchAll({
-        type: 'window',
-        includeUncontrolled: true
-    }).then(function (clientList) {
-        for (var i = 0; i < clientList.length; i++) {
-            var client = clientList[i];
-            if (client.url === urlToOpen && 'focus' in client) { // Focalizar pestaÃ±a existente
-                return client.focus();
-            }
-        }
-
-        if (clients.openWindow) { // Abrir nueva pestaÃ±a
-            return clients.openWindow(urlToOpen);
-        }
-    }));
+// fetch events
+self.addEventListener('fetch', evt => {
+    if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+        evt.respondWith(
+            caches.match(evt.request).then(cacheRes => {
+                return cacheRes || fetch(evt.request).then(fetchRes => {
+                    return caches.open(dynamicCacheName).then(cache => {
+                        cache.put(evt.request.url, fetchRes.clone());
+                        // check cached items size
+                        limitCacheSize(dynamicCacheName, 15);
+                        return fetchRes;
+                    })
+                });
+            }).catch(() => {
+                if(evt.request.url.indexOf('.html') > -1){
+                    return caches.match('/pages/fallback.html');
+                }
+            })
+        );
+    }
 });
