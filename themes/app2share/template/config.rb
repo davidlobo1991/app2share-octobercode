6ffddffeb2c1fe85466835aaf36c220241d86fbb
David Gonzalez <david_gc_@hotmail.com>
2019-09-27 00:53:39 +0200

    Started template

---
+++ themes/app2share/template/config.rb
@@ -0,0 +1,16 @@
+environment		= :production
+project_type	= :stand_alone
+http_path		= "/"
+css_dir			= "css"
+sass_dir		= "scss"
+images_dir		= "images"
+relative_assets	= true
+sass_options	= {
+	:line_numbers => true,
+	:debug_info   => false
+}
+output_style	=:expanded
+# output_style	= :compact
+# output_style	= :expanded
+
+#Encoding.default_external = 'utf-8'
