9e2f7dc1917bb74294a4e34b5cb504f1bec288bb
Arturo Tugores <arturo.tugores@app2share.es>
2019-12-05 16:38:40 +0100

    Estilos ofertas

--- themes/app2share/template/js/functions.js
+++ themes/app2share/template/js/functions.js
@@ -8,7 +8,7 @@ $(document).on('ready', function () {
     });
     $('.slider-nav').slick({
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         asNavFor: '.slider-for',
         dots: true,
         centerMode: true,
