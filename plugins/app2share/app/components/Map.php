e76367ed2aaa9608dca5779fe8c59e23e27f4dcd
David Gonzalez | Refineria Web <dgonzalez@refineriaweb.com>
2020-04-19 20:58:41 +0200

    Filtros en mapa, imagenes y lista

--- plugins/app2share/app/components/Map.php
+++ plugins/app2share/app/components/Map.php
@@ -4,6 +4,7 @@ use App2share\App\Models\Offer;
 use App2share\App\Models\Partner;
 use App2share\App\Models\PartnerType;
 use Cms\Classes\ComponentBase;
+use Illuminate\Support\Facades\Config;
 use function foo\func;
 
 class Map extends ComponentBase
@@ -28,6 +29,19 @@ class Map extends ComponentBase
             ->where('active', 1)
             ->orderBy('name', 'asc')
             ->get();
+
+        foreach($offers as $offer) {
+            $logoPartner = $offer->partner->logo;
+            $pathLogoPartner = null;
+
+            if ($logoPartner) {
+                $pathLogoPartner = $logoPartner->getPath();
+            } else {
+                $pathLogoPartner = url('/') . '/themes/app2share/assets/image/imagenot.jpg';
+            }
+
+            $offer->partner['image'] = $pathLogoPartner;
+        }
 
         $partnerTypes = PartnerType::all();
 
